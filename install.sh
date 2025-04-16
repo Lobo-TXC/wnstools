5dea9aed51d026456c397016d50b95fe48a4f21f
c0y0t3 <c0y0t3@localhost.localdomain>
2025-04-16 01:55:40 -0300

    Primer adicion de un archivo que comprueba que tenemos instalados los paquetes necesarios

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+if which python3 >/dev/null 2>&1 ; then
+    echo "Python ya se encuentra instalado"
+else 
+    echo "Iniciando instalacion de python..."
+    sudo apt -y install python3
+fi
+
+if which nmap >/dev/null 2>&1 ; then
+    echo "Nmap ya se encuentra instalado"
+else 
+    echo "Iniciando instalacion de nmap..."
+    sudo apt -y install nmap
+fi

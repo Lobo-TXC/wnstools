38d6e5662e111a5d0ec61709b72d3441c1300908
c0y0t3 <c0y0t3@localhost.localdomain>
2025-04-17 12:59:25 -0300

    Se añadio un comando de instalacion para arch y ubuntu y se definieron nombres individuales para cada uno

---
+++ install-arch.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+if which python3 >/dev/null 2>&1 ; then
+    echo "Python ya se encuentra instalado"
+else 
+    echo "Iniciando instalacion de python..."
+    sudo pacman -S install python3 --noconfirm
+fi
+
+if which nmap >/dev/null 2>&1 ; then
+    echo "Nmap ya se encuentra instalado"
+else 
+    echo "Iniciando instalacion de nmap..."
+    sudo pacman -S install nmap --noconfirm
+fi
+
+echo 'Instalando comandos necesarios...'
+
+ cp Scripts/* /usr/local/bin
+
+echo 'Definiendo los comandos...'
+
+alias wnscan="/usr/local/bin/wnscan"
+sudo chmod +x /usr/local/bin/wnscan
---
+++ install-ubuntu.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+if which python3 >/dev/null 2>&1 ; then
+    echo "Python ya se encuentra instalado"
+else 
+    echo "Iniciando instalacion de python..."
+    sudo apt-get -y install python3
+fi
+
+if which nmap >/dev/null 2>&1 ; then
+    echo "Nmap ya se encuentra instalado"
+else 
+    echo "Iniciando instalacion de nmap..."
+    sudo apt-get -y install nmap
+fi
+
+echo 'Instalando comandos necesarios...'
+
+ cp Scripts/* /usr/local/bin
+
+echo 'Definiendo los comandos...'
+
+alias wnscan="/usr/local/bin/wnscan"
+sudo chmod +x /usr/local/bin/wnscan
